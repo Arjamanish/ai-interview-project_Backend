b5afa324341cb1201fe7e9c2f88e15c254376ed1
Arja Manish <arjamanish5@gmail.com>
2026-07-30 14:39:15 +0530

    Fix resume PDF generation using Sparticuz Chromium

--- src/services/resume.service.js
+++ src/services/resume.service.js
@@ -1,50 +1,67 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
+
 const { renderResumeHtml } = require("./resume.template");
 
+
 async function htmlToPdfBuffer(html) {
-  let browser;
+    let browser;
 
-  try {
-    browser = await puppeteer.launch({
-      executablePath: puppeteer.executablePath(),
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-      ],
-    });
+    try {
 
-    const page = await browser.newPage();
+        const executablePath = await chromium.executablePath();
 
-    await page.setContent(html, {
-      waitUntil: "networkidle0",
-    });
+        console.log("USING SPARTICUZ CHROMIUM");
+        console.log("Chromium path:", executablePath);
 
-    const pdf = await page.pdf({
-      format: "A4",
-      printBackground: true,
-      margin: {
-        top: "0",
-        right: "0",
-        bottom: "0",
-        left: "0",
-      },
-    });
+        browser = await puppeteer.launch({
+            executablePath,
+            args: chromium.args,
+            headless: true,
+        });
 
-    return Buffer.from(pdf);
-  } finally {
-    if (browser) {
-      await browser.close();
+
+        const page = await browser.newPage();
+
+        await page.setContent(html, {
+            waitUntil: "networkidle0",
+        });
+
+
+        const pdf = await page.pdf({
+            format: "A4",
+            printBackground: true,
+            margin: {
+                top: "0",
+                right: "0",
+                bottom: "0",
+                left: "0",
+            },
+        });
+
+
+        return Buffer.from(pdf);
+
+
+    } finally {
+
+        if (browser) {
+            await browser.close();
+        }
+
     }
-  }
 }
+
 
 async function buildResumePdf(resumeData) {
-  const html = renderResumeHtml(resumeData);
-  return htmlToPdfBuffer(html);
+
+    const html = renderResumeHtml(resumeData);
+
+    return htmlToPdfBuffer(html);
+
 }
 
+
 module.exports = {
-  buildResumePdf,
+    buildResumePdf,
 };
